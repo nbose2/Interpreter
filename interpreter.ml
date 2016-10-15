55d6d48ea8e289cefb05b143bddbd3014bae4531
ShuyangLiu <sliu44@u.rochester.edu>
2016-10-15 16:49:31 -0400

    add parentheses

--- interpreter.ml
+++ interpreter.ml
@@ -662,10 +662,10 @@ and interpret_sl (sl:ast_sl) (mem:memory)
   | s::[] -> interpret_s s mem inp outp
   | s::ssl 
         -> let res = interpret_s s mem inp outp in
-                match res with
+                (match res with
                 | (stat , memo , input , output) 
                         -> interpret_sl ssl memo input output
-                | _ -> raise (Failure "interpret_sl: cannot interpret erroneous tree")
+                | _ -> raise (Failure "interpret_sl: cannot interpret erroneous tree"))
   | [] -> (Good, mem, inp, outp)
   | _ -> raise (Failure "interpret_sl: cannot interpret erroneous tree")
 
